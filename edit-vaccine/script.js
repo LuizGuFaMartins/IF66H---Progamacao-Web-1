26a03ca1b1c6b2b6a42fe8d52240a1c51abca337
Luiz <luizgufamartins@gmail.com>
2022-10-05 13:59:52 -0300

    feat: add edit screen content

--- edit-vaccine/script.js
+++ edit-vaccine/script.js
@@ -1,25 +1,25 @@
 document
-.getElementById("open-modal-button")
-.addEventListener("click", (event) => {
-  const modal = document.getElementById("modal-background");
-  modal.style.display = "flex";
-});
+  .getElementById("open-modal-button")
+  .addEventListener("click", (event) => {
+    const modal = document.getElementById("modal-background");
+    modal.style.display = "flex";
+  });
 
 document
-.getElementById("close-modal")
-.addEventListener("click", (event) => {
-  const modal = document.getElementById("modal-background");
-  modal.style.display = "none";
-});
-
-document
-.getElementById("modal-background")
-.addEventListener("click", (event) => {
-  const modal = document.getElementById("modal-background");
-  if (event.target === modal) {
+  .getElementById("close-modal")
+  .addEventListener("click", (event) => {
+    const modal = document.getElementById("modal-background");
     modal.style.display = "none";
-  }
-});
+  });
+
+document
+  .getElementById("modal-background")
+  .addEventListener("click", (event) => {
+    const modal = document.getElementById("modal-background");
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
 
 window.onload = () => {
   const selectedVaccine = JSON.parse(localStorage.getItem('selected_vaccine'));
@@ -30,8 +30,21 @@ window.onload = () => {
   const vaccine = document.getElementById('name');
   vaccine.value = selectedVaccine.nome;
 
-  // const dose = document.getElementById('name');
-  // vaccine.value = selectedVaccine.nome;
+  if (selectedVaccine.dose === '1a. dose') {
+    document.getElementById('primeira').checked = true;
+  }
+  if (selectedVaccine.dose === '2a. dose') {
+    document.getElementById('segunda').checked = true;
+  }
+  if (selectedVaccine.dose === '3a. dose') {
+    document.getElementById('terceira').checked = true;
+  }
+  if (selectedVaccine.dose === 'Reforço') {
+    document.getElementById('reforco').checked = true;
+  }
+  if (selectedVaccine.dose === 'Dose única') {
+    document.getElementById('unica').checked = true;
+  }
 
   // const image = document.getElementById('name');
   // vaccine.value = selectedVaccine.nome;
--- home/script.js
+++ home/script.js
@@ -52,19 +52,35 @@ const addClickEventOnCards = (temporaryCards) => {
     }
 }
 
+const formatDate = (date) => {
+    if (date.includes('Próxima dose:')) {
+        date = date.split(': ')[1];
+        day = date.split('-')[2];
+        month = date.split('-')[1];
+        year = date.split('-')[0];
+        return `Próxima dose: ${day}/${month}/${year}`;
+    } else {
+        day = date.split('-')[2];
+        month = date.split('-')[1];
+        year = date.split('-')[0];
+        return `${day}/${month}/${year}`;
+    }
+
+}
+
 window.onload = () => {
     const vaccineList = document.getElementById("vaccine-list");
     const temporaryCards = [
         {
             nome: 'BCG',
-            dose: 'Primeira',
+            dose: '1a. dose',
             data: '2022-09-09',
             urlImagem: '../assets/initial_background.jpg',
             proxima: 'Próxima dose: 2025-09-20',
         },
         {
             nome: 'Hepatite',
-            dose: 'Segunda',
+            dose: '2a. dose',
             data: '2022-09-09',
             urlImagem: '../assets/initial_background.jpg',
             proxima: 'Próxima dose: 2025-09-20',
@@ -78,12 +94,19 @@ window.onload = () => {
         },
         {
             nome: 'Rotavírus',
-            dose: 'Primeira',
+            dose: '3a. dose',
             data: '2022-09-09',
             urlImagem: '../assets/initial_background.jpg',
             proxima: 'Próxima dose: 2025-09-20',
         },
-        
+        {
+            nome: 'Covid',
+            dose: 'Dose única',
+            data: '2022-09-09',
+            urlImagem: '../assets/initial_background.jpg',
+            proxima: 'Próxima dose: 2025-09-20',
+        },
+
     ]
 
     if (temporaryCards.length <= 2) {
@@ -92,7 +115,7 @@ window.onload = () => {
     }
 
     temporaryCards.forEach((card, index) => {
-        vaccineList.appendChild(createCard(index, card.nome, card.dose, card.data, card.urlImagem, card.proxima));
+        vaccineList.appendChild(createCard(index, card.nome, card.dose, formatDate(card.data), card.urlImagem, formatDate(card.proxima)));
     })
 
     addClickEventOnCards(temporaryCards);
