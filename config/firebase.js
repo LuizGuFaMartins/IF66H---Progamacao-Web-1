ecd900f7a3d716bcd188988d449a51a62478ca28
Luiz <luizgufamartins@gmail.com>
2022-11-21 21:57:14 -0300

    feat: add vaccine form

--- config/firebase.js
+++ config/firebase.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-import {getAuth} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+import { initializeFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXhk-jBa_G-0BkdW911s5SKyQSFfv6yVg",
-    authDomain: "if66h-3fde8.firebaseapp.com",
-    projectId: "if66h-3fde8",
-    storageBucket: "if66h-3fde8.appspot.com",
-    messagingSenderId: "551995125054",
-    appId: "1:551995125054:web:54f6ba846b292ae536906f",
-    measurementId: "G-VRZTPL1YX7"
-  };
+  apiKey: "AIzaSyDXhk-jBa_G-0BkdW911s5SKyQSFfv6yVg",
+  authDomain: "if66h-3fde8.firebaseapp.com",
+  projectId: "if66h-3fde8",
+  storageBucket: "if66h-3fde8.appspot.com",
+  messagingSenderId: "551995125054",
+  appId: "1:551995125054:web:54f6ba846b292ae536906f",
+  measurementId: "G-VRZTPL1YX7"
+};
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
+const db = initializeFirestore(app, { experimentalForceLongPolling: true });
 
-export {app, auth}
+export { app, auth, db }
--- new-vaccine/script.js
+++ new-vaccine/script.js
@@ -1,6 +1,129 @@
-document.querySelector('button[type=submit]').addEventListener('click', (event) => {
-    event.preventDefault();
+import { db } from '../config/firebase.js';
+import { addDoc, collection, getDoc, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { uploadBytes, ref, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js";
 
+let file = null;
+
+window.onload = () => {
+    document.querySelector('button[type=submit]').addEventListener('click', (event) => {
+        event.preventDefault();
+        if (validateFields()) {
+            register();
+        }
+
+    })
+
+    document.getElementById('arquivo').addEventListener('change', (event) => {
+        file = event.target.files[0];
+        const img = document.getElementsByClassName('image-vaccine')[0];
+        img.setAttribute('src', URL.createObjectURL(file));
+        console.log(file)
+    })
+
+    // const uid = () => {
+    //     const id = Date.now().toString(16) + Math.random().toString(16)
+    //     return id.replace(/\./g, '')
+    // }
+
+    // const fileRef = "images/" + uid()
+    // uploadBytes(ref(storage, fileRef), file)
+    // .then((result) => {
+    //     console.log("Arquivo enviado com sucesso: " + result)
+    //     getDownloadURL(ref(storage, fileRef))
+    //         .then((url) => {
+    //             console.log("URL: " + url)
+    //             addDoc(collection(db, "alunos"), {
+    //                 nome: getNome(),
+    //                 dataNascimento: getDataNascimento(),
+    //                 urlFoto: url,
+    //                 pathFoto: fileRef
+    //             })
+    //                 .then((result) => {
+    //                     console.log(JSON.stringify(result))
+    //                     window.location.href = "home.html"
+    //                 })
+    //                 .catch((error) => {
+    //                     console.log("Erro ao persistir dados: " + error)
+    //                 })
+    //         })
+    //         .catch((error) => {
+    //             console.log("Erro ao recuperar URL de download: " + error)
+    //         })
+    // })
+    // .catch((error) => {
+    //     console.log("Erro ao enviar arquivo: " + error)
+    // })
+
+
+
+    // const id = new URLSearchParams(window.location.search).get('id')
+
+    // document.getElementById("btnSalvar").addEventListener('click', () => {salvar(id)})
+    // document.getElementById("btnExcluir").addEventListener('click', () => {excluir(id)})
+
+    // if(id) {
+
+    //     document.getElementById("btnCadastrar").style.display = 'none'
+
+    //     getDoc(doc(db, "alunos", id))
+    //     .then((documento) => {
+    //         setNome(documento.data().nome)
+    //         setDataNascimento(documento.data().dataNascimento)
+    //         setUrlFoto(documento.data().urlFoto)
+    //     })
+    //     .catch((error) => {
+    //         console.log("Erro ao recuperar o documento: " + error)
+    //     })
+    // } else {
+    //     document.getElementById("btnSalvar").style.display = 'none'
+    //     document.getElementById("btnExcluir").style.display = 'none'
+    // }
+
+}
+
+
+
+const getDateVaccine = () => {
+    return document.getElementById("date-vac").value
+}
+
+const getVaccine = () => {
+    return document.getElementById("vaccine").value
+}
+
+const getDose = () => {
+    return document.getElementById("txtFoto").value
+}
+
+const getVaccineDocumentUrl = () => {
+    return document.getElementById("txtFoto").value
+}
+
+const getNextVaccine = () => {
+    return document.getElementById("next-date-vaccine").value
+}
+
+const setDateVaccine = (nome) => {
+    document.getElementById("date-vac").value = nome
+}
+
+const setVaccine = (data) => {
+    document.getElementById("vaccine").value = data
+}
+
+const setDose = (url) => {
+    document.getElementById("txtFoto").value = url
+}
+
+const setVaccineDocumentUrl = (url) => {
+    document.getElementById("txtFoto").value = url
+}
+
+const setNextVaccinet = (url) => {
+    document.getElementById("next-date-vaccine").value = url
+}
+
+const validateFields = () => {
     let isFormValid = true;
 
     // validação de todos os campos
@@ -32,9 +155,48 @@ document.querySelector('button[type=submit]').addEventListener('click', (event) 
         } else {
             radioBox.classList.remove('input-radio-error-class');
         }
-    });   
+    });
 
-    if (isFormValid) {
-        window.location.href = "../home";
-    }
-})
+    return isFormValid;
+}
+
+const register = () => {
+    addDoc(collection(db, "vacinas"), {
+        data_vacinacao: getDateVaccine(),
+        // dose: getDose(),
+        dose: "primeira",
+        proxima_vacinacao: getNextVaccine(),
+        url_comprovate: "url",
+        // url_comprovate: getVaccineDocumentUrl(),
+        vacina: getVaccine(),
+    }).then((result) => {
+        console.log(JSON.stringify(result))
+        // window.location.href = "../home";
+    }).catch((error) => {
+        console.log("Erro ao persistir dados: " + error)
+    })
+}
+
+const remove = (id) => {
+    deleteDoc(doc(db, "alunos", id))
+        .then(() => {
+            window.location.href = "home.html"
+        })
+        .catch((error) => {
+            console.log("Erro ao excluir documento: " + error)
+        })
+}
+
+const save = (id) => {
+    updateDoc(doc(db, "alunos", id), {
+        nome: getNome(),
+        dataNascimento: getDataNascimento(),
+        urlFoto: getUrlFoto()
+    })
+        .then(() => {
+            window.location.href = "home.html"
+        })
+        .catch((error) => {
+            console.log("Erro ao atualizar o documento: " + error)
+        })
+}
